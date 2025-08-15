7f88d4d09d9a8933b2c199dc09fe0014f552eab7
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-08-15 15:42:47 -0300

    new color

--- src/modals/Prompt.tsx
+++ src/modals/Prompt.tsx
@@ -61,7 +61,7 @@ export default function Prompt(props: PromptProps) {
               </button>
               <button
                 type="submit"
-                className="cursor-pointer inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 ml-3 w-auto text-sm"
+                className="cursor-pointer inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ml-3 w-auto text-sm"
               >
                 Confirmar
               </button>
