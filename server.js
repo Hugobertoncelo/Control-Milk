1fa0dba24a8c61faeb16a9e6467f45a9a28c6d69
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-11-12 17:15:12 -0300

    preparing for deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+const PORT = process.env.PORT || 10000;
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`);
+});
