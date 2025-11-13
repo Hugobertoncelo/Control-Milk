42f69bf2bf43464b7226280d144e2e64db0c2258
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-11-13 09:42:27 -0300

    new server.js

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 
 const app = express();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-app.use(cors({
-  origin: "*",
-}));
+app.use(cors());
+app.use(express.json());
 
 app.use("/api", middlewares, router);
 
@@ -20,4 +19,6 @@ app.get("*", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`🚀 Server rodando na porta ${PORT}`);
+});
